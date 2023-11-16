5ebd04c82704ea87d41d47584ce6d9fbddcf611a
Carlos Paz <147278027+carlos-paz12@users.noreply.github.com>
2023-11-16 15:59:31 -0300

    Commit inicial

---
+++ src/test/java/com/github/carlos_paz12/exercicios_springboot/exercicios_springboot/ExerciciosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.carlos_paz12.exercicios_springboot.exercicios_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
